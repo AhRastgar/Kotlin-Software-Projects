bd6749ba921f727400f41307fbb39003a01a31e7
AhRastgar <ah.rastgar.c@gmail.com>
2022-05-24 09:20:33 +0330

    Draw triangle

--- Draw triangle/app/src/main/java/com/example/drawrectangle/MainActivity.kt
+++ Draw triangle/app/src/main/java/com/example/drawrectangle/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.drawrectangle
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
