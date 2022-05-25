86964187c5168b0fc57c1eccd932cf91edd77cca
AhRastgar <ah.rastgar.c@gmail.com>
2022-05-25 15:25:48 +0330

    Complete number

--- Complete number/app/src/main/java/com/example/completenumber/MainActivity.kt
+++ Complete number/app/src/main/java/com/example/completenumber/MainActivity.kt
@@ -2,12 +2,42 @@ package com.example.completenumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bt = findViewById<Button>(R.id.bt)
+        val tn = findViewById<TextView>(R.id.tn)
 
+        bt.setOnClickListener()
+        {
+            val x = tn.text.toString().toInt()
+            var num = x
+            var sum = 0
+            var i = 1
+            
+            while (i <= x)
+            {
+                if (x % i == 0)
+                {
+                    sum += i
+                }
+                i++
+            }
+            
+            if (sum == num)
+            {
+                Toast.makeText(this, "This number is complete.", Toast.LENGTH_LONG).show()
+            }
+            else
+            {
+                Toast.makeText(this, "This number is not compete.", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
