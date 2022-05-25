120bff578b88ffd6822d6a5d9c7a1af6cc6d430c
AhRastgar <ah.rastgar.c@gmail.com>
2022-05-25 11:20:05 +0330

    Draw square

--- DrawSquare/app/src/main/java/com/example/drawsquare/CanvasView.kt
+++ DrawSquare/app/src/main/java/com/example/drawsquare/CanvasView.kt
@@ -1,4 +1,17 @@
 package com.example.drawsquare
 
-class CanvasView {
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class CanvasView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr)
+{
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        
+    }
 }
