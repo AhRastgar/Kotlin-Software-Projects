1627d7bd54f0376ad8afceed771b2d34eafcd3cd
AhRastgar <ah.rastgar.c@gmail.com>
2022-05-25 16:15:59 +0330

    Pi

--- Pi/app/src/main/java/com/example/pi/MainActivity.kt
+++ Pi/app/src/main/java/com/example/pi/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.pi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tn = findViewById<TextView>(R.id.tn)
+        val bt = findViewById<Button>(R.id.bt)
+
+        bt.setOnClickListener()
+        {
+            var x = tn.text.toString().toInt()
+            var p = 3.14
+            var stash = x * p * 2
+            var pi = stash / x
+
+            Toast.makeText(this, "pi: $pi", Toast.LENGTH_LONG).show()
+        }
     }
 }
