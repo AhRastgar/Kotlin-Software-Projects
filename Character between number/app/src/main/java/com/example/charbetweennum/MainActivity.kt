921c5ab6606cb0e3a481babc5b8b11c3c9ad20a1
AhRastgar <ah.rastgar.c@gmail.com>
2022-05-27 09:13:10 +0330

    Character between number

--- Character between number/app/src/main/java/com/example/charbetweennum/MainActivity.kt
+++ Character between number/app/src/main/java/com/example/charbetweennum/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.charbetweennum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tn = findViewById<TextView>(R.id.tn)
+        val tc = findViewById<TextView>(R.id.tc)
+        val bt = findViewById<Button>(R.id.bt)
+
+        bt.setOnClickListener()
+        {
+            val a = tn.text.toString().toInt()
+            val b = tc.text.toString()
+            var sum = 0
+            var mod = 0
+            var rev = 0
+            var i = 0
+            var num = a
+            var j = 0
+
+            while (i < a)
+            {
+                mod = num % 10
+                rev = (rev * 10) + mod
+                num /= 10
+            }
+
+            while (j < a)
+            {
+                mod = rev % 10
+                rev = (rev * 10) + mod
+
+                Toast.makeText(this, "$rev", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
