ddf5c469dc2ae4276f97b9bfbe7b182d800a311c
AhRastgar <ah.rastgar.c@gmail.com>
2022-05-31 16:14:58 +0330

    Prime number

--- Prime number/app/src/main/java/com/example/primenumber/MainActivity.kt
+++ Prime number/app/src/main/java/com/example/primenumber/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.primenumber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bt = findViewById<Button>(R.id.bt)
+        val tn = findViewById<TextView>(R.id.tn)
+
+        bt.setOnClickListener()
+        {
+            val x = tn.text.toString().toInt()
+            var i = 0
+            var test = 0
+
+            while (i < x)
+            {
+                if (x % i == 0)
+                {
+                    test = 1
+                    break
+                }
+                i++
+            }
+
+            if (test == 0)
+            {
+                Toast.makeText(this, "This is prime number", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Toast.makeText(this, "That is not prime number", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
